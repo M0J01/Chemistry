8a20df5a29281fff7ad3dcbedc522419266f7047
M0J01 <moodymusicman@gmail.com>
2015-08-25 01:16:30 -0400

    Put a list as the input to the element class, and fed it in a list when declaring new elements. Something I still do not know how to do is to take a list of intended variable names, and declare them when they are relevant... I could declare them ahead of time, however that would not be scalable.

--- List_Reader.py
+++ List_Reader.py
@@ -1,4 +1,21 @@
 
+class element:
+	def __init__(self, in_List): #list[self, symbol, aomtic_Number, name, conductivity, amu, electro_Neg, melt_P, boil_P, group, block, ionization_Energy, element_Category]
+		self.symbol = in_List[0]						#For the English American Periodic Table
+		self.atomic_Number = in_List[1]			#
+		self.name = in_List[2]							#
+		self.conductivity = in_List[3] 			# Gasses don't conduct very well under STP. Need a way to declare as NA... Hydrogen has a huge scale of conduction under various situations
+		self.amu = in_List[4]								#
+		self.electro_Neg = in_List[5] 				#Pauling Scale
+		self.melt_P = in_List[6]						#Kelvin at STP
+		self.boil_P = in_List[7] 						#Kelvin at STP
+		self.group = in_List[8]							#Periodic Table as of 8/24/15
+		self.block = in_List[9]							#Periodic Table as of 8/24/15
+		self.ionization_Energy = in_List[10] 	#Kj/mol
+		self.element_Category = in_List[11] 	#Metalloid
+
+
+
 elements = []
 temp = []
 									#Holds string of e_L
@@ -23,8 +40,9 @@ for entry in e_L_3:							#creating a new container with one list of attributes 
 #print temp
 
 for item in temp:
-	elements.append(item)
-	
+	elements.append(element(item))
+
+print elements[2].name
 	
 
 
